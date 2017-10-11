f35330563e6975808ae063ff7058507cf8aef532
mrkkrp <markkarpov92@gmail.com>
2017-10-11 19:12:59 +0700

    Add a simple outline of the test suite

--- tests/Text/MMark/ExtensionSpec.hs
+++ tests/Text/MMark/ExtensionSpec.hs
@@ -1,6 +1,15 @@
 module Text.MMark.ExtensionSpec (spec) where
 
 import Test.Hspec
+-- import Text.MMark.Extension
 
 spec :: Spec
-spec = return ()
+spec = do
+  describe "blockTrans" $
+    it "" pending -- TODO
+  describe "blockRender" $
+    it "" pending -- TODO
+  describe "inlineTrans" $
+    it "" pending -- TODO
+  describe "inlineRender" $
+    it "" pending -- TODO
--- tests/Text/MMarkSpec.hs
+++ tests/Text/MMarkSpec.hs
@@ -3,4 +3,17 @@ module Text.MMarkSpec (spec) where
 import Test.Hspec
 
 spec :: Spec
-spec = return ()
+spec = do
+  describe "parseMMark and renderMMark" $ do
+    it "" pending -- TODO Adjust the Common Mark spec for features that
+      -- we've implemented already.
+  describe "useExtension" $ do
+    it "" pending
+  describe "useExtensions" $ do
+    it "" pending
+  describe "runScanner and scanner" $ do
+    it "" pending
+  describe "(.&+)" $ do
+    it "" pending
+  describe "mmarkYaml" $ do
+    it "" pending
