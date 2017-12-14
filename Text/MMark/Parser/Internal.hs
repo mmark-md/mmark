5ce2f115f182c6ef47f62fac7f8d86ccc04ed365
mrkkrp <markkarpov92@gmail.com>
2017-12-14 21:52:05 +0700

    Implement reference links and reference-style images

---
+++ Text/MMark/Parser/Internal.hs
@@ -0,0 +1,303 @@
+-- |
+-- Module      :  Text.MMark.Parser.Internal
+-- Copyright   :  © 2017 Mark Karpov
+-- License     :  BSD 3 clause
+--
+-- Maintainer  :  Mark Karpov <markkarpov92@gmail.com>
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- An internal module that builds a framework on which the
+-- "Text.MMark.Parser" module is built.
+
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE RankNTypes                 #-}
+
+module Text.MMark.Parser.Internal
+  ( -- * Block-level parser monad
+    BParser
+  , runBParser
+  , isNakedAllowed
+  , refLevel
+  , subEnv
+  , registerReference
+  , registerFootnote
+    -- * Inline-level parser monad
+  , IParser
+  , runIParser
+  , disallowEmpty
+  , isEmptyAllowed
+  , disallowLinks
+  , isLinksAllowed
+  , disallowImages
+  , isImagesAllowed
+  , isLastSpace
+  , isLastOther
+  , lastSpace
+  , lastOther
+  , lookupReference
+  , lookupFootnote
+  , Isp (..)
+    -- * Reference and footnote definitions
+  , Defs
+    -- * Other
+  , MMarkErr (..) )
+where
+
+import Control.Applicative
+import Control.Monad.State.Strict
+import Data.Default.Class
+import Data.Function ((&))
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.Map.Strict (Map)
+import Data.Ratio ((%))
+import Data.Set (Set)
+import Data.Text (Text)
+import Data.Text.Metrics (damerauLevenshteinNorm)
+import Lens.Micro (Lens', (^.), (.~), set, over, to)
+import Lens.Micro.Extras (view)
+import Text.MMark.Internal
+import Text.MMark.Parser.Internal.Type
+import Text.Megaparsec hiding (State)
+import Text.URI (URI)
+import qualified Data.Map.Strict as M
+import qualified Data.Set        as E
+import qualified Text.Megaparsec as M
+
+----------------------------------------------------------------------------
+-- Block-level parser monad
+
+-- | Block-level parser type.
+
+newtype BParser a = BParser
+  { unBParser :: ParsecT MMarkErr Text (State BlockState) a }
+  deriving ( Functor
+           , Applicative
+           , Alternative
+           , Monad
+           , MonadPlus
+           , MonadParsec MMarkErr Text )
+
+-- | Run a computation in the 'BParser' monad.
+
+runBParser
+  :: BParser a
+     -- ^ The parser to run
+  -> FilePath
+     -- ^ File name (only to be used in error messages), may be empty
+  -> Text
+     -- ^ Input to parse
+  -> Either (NonEmpty (ParseError Char MMarkErr)) (a, Defs)
+     -- ^ Result of parsing
+runBParser (BParser p) file input =
+  case runState (runParserT p file input) def of
+    (Left err, _) -> Left  (err :| [])
+    (Right x, st) -> Right (x, st ^. bstDefs)
+
+-- | Ask whether naked paragraphs are allowed in this context.
+
+isNakedAllowed :: BParser Bool
+isNakedAllowed = BParser $ gets (^. bstAllowNaked)
+
+-- | Lookup current reference indentation level.
+
+refLevel :: BParser Pos
+refLevel = BParser $ gets (^. bstRefLevel)
+
+-- | Execute 'BParser' computation with modified environment.
+
+subEnv
+  :: Bool              -- ^ Whether naked paragraphs should be allowed
+  -> Pos               -- ^ Reference indentation level
+  -> BParser a         -- ^ The parser we want to set the environment for
+  -> BParser a         -- ^ The resulting parser
+subEnv allowNaked rlevel
+  = BParser
+  . locally bstAllowNaked allowNaked
+  . locally bstRefLevel   rlevel
+  . unBParser
+
+-- | Register a reference (link\/image) definition.
+
+registerReference
+  :: Text              -- ^ Reference name
+  -> (URI, Maybe Text) -- ^ Reference 'URI' and optional title
+  -> BParser Bool      -- ^ 'True' if there is a conflicting definition
+registerReference = registerGeneric referenceDefs
+
+-- | Register a footnote definition.
+
+registerFootnote
+  :: Text              -- ^ Reference name
+  -> NonEmpty Inline   -- ^ FIXME Footnote definition
+  -> BParser Bool      -- ^ 'True' if there is a conflicting definition
+registerFootnote = registerGeneric footnoteDefs
+
+-- | A generic function for registering definitions in 'BParser'.
+
+registerGeneric
+  :: Lens' Defs (Map DefLabel a) -- ^ How to access the definition map
+  -> Text              -- ^ Definition name
+  -> a                 -- ^ Data
+  -> BParser Bool      -- ^ 'True' if there is a conflicting definition
+registerGeneric l name a = BParser $ do
+  let dlabel = mkDefLabel name
+  defs <- gets (^. bstDefs . l)
+  if M.member dlabel defs
+    then return True
+    else do
+      modify' $ over (bstDefs . l) (M.insert dlabel a)
+      return False
+
+----------------------------------------------------------------------------
+-- Inline-level parser monad
+
+-- | Inline-level parser type.
+
+newtype IParser a = IParser
+  { unIParser :: StateT InlineState (Parsec MMarkErr Text) a }
+  deriving ( Functor
+           , Applicative
+           , Alternative
+           , Monad
+           , MonadPlus
+           , MonadParsec MMarkErr Text )
+
+-- | Run a computation in the 'IParser' monad.
+
+runIParser
+  :: Defs
+     -- ^ Reference and footnote definitions obtained as a result of
+     -- block-level parsing
+  -> IParser a
+     -- ^ The parser to run
+  -> Isp
+     -- ^ Input for the parser
+  -> Either (ParseError Char MMarkErr) a
+     -- ^ Result of parsing
+runIParser _ _ (IspError err) = Left err
+runIParser defs (IParser p) (IspSpan startPos input) =
+  snd (runParser' (evalStateT p ist) pst)
+  where
+    ist = def & istDefs .~ defs
+    pst = M.State
+      { stateInput           = input
+      , statePos             = startPos :| []
+      , stateTokensProcessed = 0
+      , stateTabWidth        = mkPos 4
+      }
+
+-- | Disallow parsing of empty inlines.
+
+disallowEmpty :: IParser a -> IParser a
+disallowEmpty = IParser . locally istAllowEmpty False . unIParser
+
+-- | Ask whether parsing of empty inlines is allowed.
+
+isEmptyAllowed :: IParser Bool
+isEmptyAllowed = IParser . gets $ view istAllowEmpty
+
+-- | Disallow parsing of links.
+
+disallowLinks :: IParser a -> IParser a
+disallowLinks = IParser . locally istAllowLinks False . unIParser
+
+-- | Ask whether parsing of links is allowed.
+
+isLinksAllowed :: IParser Bool
+isLinksAllowed = IParser . gets $ view istAllowLinks
+
+-- | Disallow parsing of images.
+
+disallowImages :: IParser a -> IParser a
+disallowImages = IParser . locally istAllowImages False . unIParser
+
+-- | Ask whether parsing of images is allowed.
+
+isImagesAllowed :: IParser Bool
+isImagesAllowed = IParser . gets $ view istAllowImages
+
+-- | Ask whether the last seen char type was space.
+
+isLastSpace :: IParser Bool
+isLastSpace = IParser . gets $ view (istLastChar . to (== SpaceChar))
+
+-- | Ask whether the last seen char type was “other” (not space).
+
+isLastOther :: IParser Bool
+isLastOther = IParser . gets $ view (istLastChar . to (== OtherChar))
+
+-- | Register that the last seen char type is space.
+
+lastSpace :: IParser ()
+lastSpace = IParser . modify' $ set istLastChar SpaceChar
+{-# INLINE lastSpace #-}
+
+-- | Register that the last seen char type is “other” (not space).
+
+lastOther :: IParser ()
+lastOther = IParser . modify' $ set istLastChar OtherChar
+{-# INLINE lastOther #-}
+
+-- | Lookup a link\/image reference definition.
+
+lookupReference
+  :: Text
+     -- ^ Reference name
+  -> IParser (Either [Text] (URI, Maybe Text))
+     -- ^ A collection of suggested reference names in 'Left' (typo
+     -- corrections) or the requested definition in 'Right'
+lookupReference = lookupGeneric referenceDefs
+
+-- | Lookup a footnote definition.
+
+lookupFootnote
+  :: Text
+     -- ^ Footnote name
+  -> IParser (Either [Text] (NonEmpty Inline))
+     -- ^ A collection of suggested reference names in 'Left' (typo
+     -- corrections) or the requested definition in 'Right'
+lookupFootnote = lookupGeneric footnoteDefs
+
+-- | A generic function for looking up definition in 'IParser'.
+
+lookupGeneric
+  :: Lens' Defs (Map DefLabel a)
+     -- ^ How to access the definition map
+  -> Text
+     -- ^ Definition name
+  -> IParser (Either [Text] a)
+     -- ^ A collection of suggested reference names in 'Left' (typo
+     -- corrections) or the requested definition in 'Right'
+lookupGeneric l name = IParser $ do
+  let dlabel = mkDefLabel name
+  defs <- gets (view (istDefs . l))
+  case M.lookup dlabel defs of
+    Nothing -> return . Left $ closeNames dlabel (M.keysSet defs)
+    Just  x -> return (Right x)
+
+-- | Select close enough (as per the Damerau-Levenshtein metric) definition
+-- labels.
+
+closeNames :: DefLabel -> Set DefLabel -> [Text]
+closeNames r'
+  = E.toList
+  . E.filter (\x -> damerauLevenshteinNorm r x >= (2 % 3))
+  . E.map unDefLabel
+  where
+    r = unDefLabel r'
+
+----------------------------------------------------------------------------
+-- Helpers
+
+-- | Locally change state in a state monad and then restore it back.
+
+locally :: MonadState s m => Lens' s a -> a -> m b -> m b
+locally l x m = do
+  y <- gets (^. l)
+  modify' (set l x)
+  r <- m
+  modify' (set l y)
+  return r
---
+++ Text/MMark/Parser/Internal/Type.hs
@@ -0,0 +1,225 @@
+-- |
+-- Module      :  Text.MMark.Parser.Internal.Type
+-- Copyright   :  © 2017 Mark Karpov
+-- License     :  BSD 3 clause
+--
+-- Maintainer  :  Mark Karpov <markkarpov92@gmail.com>
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Types for the internal helper definitions for the parser.
+
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE LambdaCase         #-}
+{-# LANGUAGE TemplateHaskell    #-}
+
+module Text.MMark.Parser.Internal.Type
+  ( -- * Block-level parser state
+    BlockState
+  , bstAllowNaked
+  , bstRefLevel
+  , bstDefs
+    -- * Inline-level parser state
+  , InlineState
+  , istLastChar
+  , istAllowEmpty
+  , istAllowLinks
+  , istAllowImages
+  , istDefs
+  , Isp (..)
+  , CharType (..)
+    -- * Reference and footnote definitions
+  , Defs
+  , referenceDefs
+  , footnoteDefs
+  , DefLabel
+  , mkDefLabel
+  , unDefLabel
+    -- * Other
+  , MMarkErr (..) )
+where
+
+import Control.DeepSeq
+import Data.CaseInsensitive (CI)
+import Data.Data (Data)
+import Data.Default.Class
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.Map.Strict (Map)
+import Data.Semigroup ((<>))
+import Data.Text (Text)
+import Data.Typeable (Typeable)
+import GHC.Generics
+import Lens.Micro.TH
+import Text.MMark.Internal
+import Text.Megaparsec
+import Text.URI (URI)
+import qualified Data.CaseInsensitive as CI
+import qualified Data.List.NonEmpty   as NE
+import qualified Data.Map.Strict      as M
+import qualified Data.Text            as T
+
+----------------------------------------------------------------------------
+-- Block-level parser state
+
+-- | Block-level parser state.
+
+data BlockState = BlockState
+  { _bstAllowNaked :: Bool
+    -- ^ Should we consider a paragraph that does not end with a blank line
+    -- 'Naked'? It does not make sense to do so for top-level document, but
+    -- in lists, 'Naked' text is pretty common.
+  , _bstRefLevel :: Pos
+    -- ^ Current reference level: 1 column for top-level of document, column
+    -- where content starts for block quotes and lists.
+  , _bstDefs :: Defs
+    -- ^ Reference and footnote definitions
+  }
+
+instance Default BlockState where
+  def = BlockState
+    { _bstAllowNaked = False
+    , _bstRefLevel   = pos1
+    , _bstDefs       = def
+    }
+
+----------------------------------------------------------------------------
+-- Inline-level parser state
+
+-- | Inline-level parser state.
+
+data InlineState = InlineState
+  { _istLastChar :: !CharType
+    -- ^ Type of the last encountered character
+  , _istAllowEmpty :: Bool
+    -- ^ Whether to allow empty inlines
+  , _istAllowLinks :: Bool
+    -- ^ Whether to allow parsing of links
+  , _istAllowImages :: Bool
+    -- ^ Whether to allow parsing of images
+  , _istDefs :: Defs
+    -- ^ Reference link definitions
+  }
+
+instance Default InlineState where
+  def = InlineState
+    { _istLastChar    = SpaceChar
+    , _istAllowEmpty  = True
+    , _istAllowLinks  = True
+    , _istAllowImages = True
+    , _istDefs        = def
+    }
+
+-- | 'Inline' source pending parsing.
+
+data Isp
+  = IspSpan SourcePos Text
+    -- ^ We have an inline source pending parsing
+  | IspError (ParseError Char MMarkErr)
+    -- ^ We should just return this parse error
+  deriving (Eq, Show)
+
+-- | Type of the last seen character.
+
+data CharType
+  = SpaceChar          -- ^ White space or a transparent character
+  | OtherChar          -- ^ Other character
+  deriving (Eq)
+
+----------------------------------------------------------------------------
+-- Reference and footnote definitions
+
+-- | An opaque container for reference and footnote definitions.
+
+data Defs = Defs
+  { _referenceDefs :: Map DefLabel (URI, Maybe Text)
+    -- ^ Reference definitions containing a 'URI' and optionally title
+  , _footnoteDefs :: Map DefLabel (NonEmpty Inline)
+    -- ^ FIXME Footnote definitions
+  }
+
+instance Default Defs where
+  def = Defs
+    { _referenceDefs = M.empty
+    , _footnoteDefs  = M.empty
+    }
+
+-- | An opaque type for definition label.
+
+newtype DefLabel = DefLabel (CI Text)
+  deriving (Eq, Ord)
+
+-- | Smart constructor for the 'DefLabel' type.
+
+mkDefLabel :: Text -> DefLabel
+mkDefLabel = DefLabel . CI.mk . T.unwords . T.words
+
+-- | Extract 'Text' value from a 'DefLabel'.
+
+unDefLabel :: DefLabel -> Text
+unDefLabel (DefLabel x) = CI.original x
+
+----------------------------------------------------------------------------
+-- Other
+
+-- | MMark custom parse errors.
+
+data MMarkErr
+  = YamlParseError String
+    -- ^ YAML error that occurred during parsing of a YAML block
+  | ListStartIndexTooBig Word
+    -- ^ Ordered list start numbers must be nine digits or less
+  | ListIndexOutOfOrder Word Word
+    -- ^ The index in an ordered list is out of order, first number is the
+    -- actual index we ran into, the second number is the expected index
+  | NonFlankingDelimiterRun (NonEmpty Char)
+    -- ^ This delimiter run should be in left- or right- flanking position
+  | DuplicateReferenceDefinition Text
+    -- ^ Duplicate reference definitions are not allowed
+  | CouldNotFindReferenceDefinition Text [Text]
+    -- ^ Could not find this reference definition, the second argument is
+    -- the collection of close names (typo corrections)
+  deriving (Eq, Ord, Show, Read, Generic, Typeable, Data)
+
+instance ShowErrorComponent MMarkErr where
+  showErrorComponent = \case
+    YamlParseError str ->
+      "YAML parse error: " ++ str
+    ListStartIndexTooBig n ->
+      "ordered list start numbers must be nine digits or less, " ++ show n
+        ++ " is too big"
+    ListIndexOutOfOrder actual expected ->
+      "list index out of order: " ++ show actual ++ ", expected "
+        ++ show expected
+    NonFlankingDelimiterRun dels ->
+      showTokens dels ++ " should be in left- or right- flanking position"
+    DuplicateReferenceDefinition name ->
+      "duplicate reference definitions are not allowed: \""
+        ++ T.unpack name ++ "\""
+    CouldNotFindReferenceDefinition name alts ->
+      "could not find a matching reference definition for \""
+        ++ T.unpack name ++ "\""
+        ++ case NE.nonEmpty alts of
+             Nothing -> ""
+             Just xs -> "\nperhaps you meant "
+               ++ orList (quote . T.unpack <$> xs) ++ "?"
+      where
+        quote x = "\"" ++ x ++ "\""
+
+instance NFData MMarkErr
+
+-- | Print a pretty list where items are separated with commas and the word
+-- “or” according to the rules of English punctuation.
+
+orList :: NonEmpty String -> String
+orList (x:|[])  = x
+orList (x:|[y]) = x <> " or " <> y
+orList xs       = intercalate ", " (NE.init xs) <> ", or " <> NE.last xs
+
+----------------------------------------------------------------------------
+-- Lens TH
+
+makeLenses ''BlockState
+makeLenses ''InlineState
+makeLenses ''Defs
